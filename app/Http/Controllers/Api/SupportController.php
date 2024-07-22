560cae57e3fefc7120b2f990de811f83a78b3689
camargo <gui2017scamargo1234@gmail.com>
2024-07-22 20:51:21 -0300

    delete API REST

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\StoreUpdateSupport;
 use App\Http\Resources\SupportResource;
 use App\Services\SupportService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SupportController extends Controller
 {
@@ -41,7 +42,14 @@ class SupportController extends Controller
      */
     public function show(string $id)
     {
-        //
+        if (!$support = $this->service->findOne($id))
+        {
+            return response()->json(
+                [
+                    'error' => 'Not found'
+                ],  Response::HTTP_NOT_FOUND); 
+        }
+        return new SupportResource($support);
     }
 
     /**
@@ -57,6 +65,16 @@ class SupportController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        if (!$this->service->findOne($id))
+        {
+            return response()->json(
+                [
+                    'error' => 'Not found'
+                ],  Response::HTTP_NOT_FOUND);
+        }
+
+        $this->service->delete($id);
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
